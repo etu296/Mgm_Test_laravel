2160f099b3feaccc887088255e6883f01517524a
D:/Cypress/Orange HRM <etumahmuda@gmail.com>
2023-07-24 17:07:29 +0600

    admin sidebar & navbar design

---
+++ resources/views/admin/partial/sidebar.blade.php
@@ -0,0 +1,114 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        
+        <div class="info">
+          <a href="#" class="d-block">Managerium</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <!-- <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div> -->
+     <!-- href="{{url('/admin/plants')}}">-->
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+               
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+              <p>
+                Purchase
+                
+              </p>
+            </a>
+          </li>
+          
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+              <p>
+                Sales
+                
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa fa-university" aria-hidden="true"></i>
+              <p>
+                Account
+                
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa fa-building" aria-hidden="true"></i>
+              <p>
+                Inventory
+                
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+              <p>
+                Production
+                
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa fa-bell" aria-hidden="true"></i>
+              <p>
+               Approval
+                
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+            <i class="fa fa-cog" aria-hidden="true"></i>
+              <p>
+                Configurations
+              </p>
+              
+            </a>
+          </li>
+          
+
+         
+
+        </ul>
+          
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
